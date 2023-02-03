1dbb2db08fdc09a04214a9cce8faeb26bf1630b4
wjdghks963 <chsw000@naver.com>
2023-02-03 09:55:55 +0900

    위치 변경

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { countSubject } from '../../store';
+	import Counter from './Counter.svelte';
+</script>
+
+<div class="counter">
+	<strong>{$countSubject}</strong>
+	<Counter />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 100vw;
+	}
+	strong {
+		font-size: 40px;
+		font-weight: bold;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/counter/Counter.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { countSubject } from '../../store';
+
+	const up = () => {
+		countSubject.next(countSubject.value + 1);
+	};
+
+	const down = () => {
+		countSubject.next(countSubject.value - 1);
+	};
+</script>
+
+<div class="counter">
+	<button on:click={up} aria-label="Decrease the counter by one"> + </button>
+	<button on:click={down} aria-label="Increase the counter by one"> - </button>
+</div>
+
+<style>
+	.counter {
+		display: flex;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+		justify-content: center;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+</style>
